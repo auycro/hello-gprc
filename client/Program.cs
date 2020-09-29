8d90efd67ca357c473d5d8e5ff962a0397f34bee
aui <g-keardkeawfa@rozetta.jp>
2020-09-30 00:15:22 +0900

    finish test connect gPRC

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Hello;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
 
 namespace client
 {
     class Program
     {
+        const string target = "127.0.0.1:5001";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Channel channel = new Channel("localhost", 5001, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new HelloService.HelloServiceClient(channel);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
         }
     }
 }
--- server/Program.cs
+++ server/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using Grpc.Core;
 
 namespace server
 {
     class Program
     {
+        const int Port = 5001;
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("The server is listening on the port : " + Port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start : " + e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+
         }
     }
 }
